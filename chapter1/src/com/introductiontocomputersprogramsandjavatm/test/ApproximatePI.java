8661cb080afde55011f2499780b3a6bacd5450d1
jack <ayimugare@gmail.com>
2023-02-20 00:13:45 +0800

    PI ===> pi

--- chapter1/src/com/introductiontocomputersprogramsandjavatm/test/ApproximatePI.java
+++ chapter1/src/com/introductiontocomputersprogramsandjavatm/test/ApproximatePI.java
@@ -9,12 +9,12 @@ public class ApproximatePI {
 
   public static void main(String[] args) {
     //  1.  display the results of 4 * (1 - 1/3 +1/5 - 1/7 + 1/9 - 1/11)
-    double PI1 = (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
-    System.out.println("PI = " + 4 * PI1);
+    double pi1 = (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
+    System.out.println("PI = " + 4 * pi1);
 
     //  2.  display the results of 4 * (1 - 1/3 +1/5 - 1/7 + 1/9 - 1/11 + 1/13).
-    double PI2 = PI1 + 1.0 / 13;
-    System.out.println("PI = " + 4 * PI2);
+    double pi2 = pi1 + 1.0 / 13;
+    System.out.println("PI = " + 4 * pi2);
   }
 
 }
